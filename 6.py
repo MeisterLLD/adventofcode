68f0af23e519edf834aa9d7f2e64f4b31b9b08a1
Laurent Dietrich <dietrich.laurent@gmail.com>
2022-12-06 19:19:33 +0100

    day 6

---
+++ 6.py
@@ -0,0 +1,28 @@
+N = 4
+#N = 14
+
+
+with open('input6','r') as f:
+    b = f.read()
+    dic = {}
+    for i in range(N):
+        if b[i] not in dic: dic[b[i]] = 1
+        else: dic[b[i]] += 1
+
+
+
+    pos = N-1
+    while len(dic) < N:
+        pos += 1
+        new = b[pos]
+        old = b[pos-N]
+
+        if old in dic:
+            if dic[old] == 1: dic.pop(old)
+            else: dic[old] -= 1
+
+        if new not in dic: dic[new] = 1
+        else: dic[new] += 1
+
+
+    print(pos+1)
