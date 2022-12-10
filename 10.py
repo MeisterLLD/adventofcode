e079208366570be9490cdc18b2b7bee7b49bce40
Laurent Dietrich <dietrich.laurent@gmail.com>
2022-12-10 08:35:02 +0100

    nettoyage de prints

--- 10.py
+++ 10.py
@@ -1,39 +1,35 @@
+detect = {20, 60, 100, 140, 180, 220}
 curcycle = 1
 X = 1
-crt = ''
-
-def actualise(crt,X,curcycle):
-    if curcycle % 40 == 1: crt += '\n'
-
-    if (curcycle-1)%40 in {X-1,X,X+1} : crt += '#'
-    else: crt += '.'
-
-    return crt
-
+power = 0
 
 for ligne in open('input10','r').readlines():
+
     if 'noop' in ligne:
         # début cycle
-        crt = actualise(crt,X,curcycle)
+        if curcycle in detect:
+            power += curcycle*X
         # fin cycle
         curcycle += 1
+
 
     elif 'addx' in ligne:
         nb = int(ligne.split(' ')[1])
 
         # début cycle
-        crt = actualise(crt,X,curcycle)
+        if curcycle in detect:
+            power += curcycle*X
         # fin cycle
         curcycle += 1
 
         # début cycle
-        crt = actualise(crt,X,curcycle)
+        if curcycle in detect:
+            power += curcycle*X
         X += nb
         # fin cycle
         curcycle += 1
 
-
-print(crt)
+print(power)
 
 
 
