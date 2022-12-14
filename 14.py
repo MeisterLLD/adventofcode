653d62134f3a0b7b305663cd666b01face231c65
Laurent Dietrich <dietrich.laurent@gmail.com>
2022-12-14 18:33:42 +0100

    nettoyage 14

--- 14.py
+++ 14.py
@@ -8,86 +8,67 @@ with open('input14','r') as fic:
     for ligne in fic.read().splitlines():
         path = ligne.split(' -> ')
         for s in path:
-            x,y = s.split(',')
-            x,y = int(x), int(y)
-            if x > xmax:
-                xmax = x
-            if x < xmin:
-                xmin = x
-            if y > ymax:
-                ymax = y
-
+            x,y = tuple(map(int, s.split(',') ))
+            if x > xmax: xmax = x
+            if x < xmin: xmin = x
+            if y > ymax: ymax = y
 n = ymax+1
 m = xmax+1
 
-# Fabrication de la map
-map = [ [0 for j in range(m)] for i in range(n+1) ]
-map[ysource][xsource] = '+'
+## Fabrication de la carte
+carte = [ [0 for j in range(m+1)] for i in range(n+1) ]
+carte[ysource][xsource] = '+'
 
 with open('input14','r') as fic:
     for ligne in fic.read().splitlines():
         path = ligne.split(' -> ')
         for i in range(len(path)-1):
-            xdeb,ydeb = path[i].split(',')
-            xdeb,ydeb = int(xdeb), int(ydeb)
-            xfin,yfin = path[i+1].split(',')
-            xfin,yfin = int(xfin), int(yfin)
+            xdeb,ydeb = tuple(map(int, path[i].split(',') ))
+            xfin,yfin = tuple(map(int, path[i+1].split(',') ))
 
             if xfin != xdeb:
-                if xfin>= xdeb:
-                    for j in range(xdeb,xfin+1):
-                        map[ydeb][j] = 1
-                else:
-                    for j in range(xfin,xdeb+1):
-                        map[ydeb][j] = 1
-
+                if xfin < xdeb: xfin, xdeb = xdeb, xfin
+                for j in range(xdeb,xfin+1):
+                    carte[ydeb][j] = 1
             elif yfin != ydeb:
-                if yfin >= ydeb:
-                    for k in range(ydeb,yfin+1):
-                        map[k][xdeb] = 1
-                else:
-                    for k in range(yfin,ydeb+1):
-                        map[k][xdeb] = 1
+                if yfin < ydeb: yfin, ydeb = ydeb, yfin
+                for k in range(ydeb,yfin+1):
+                    carte[k][xdeb] = 1
+##
 
+# def affiche(carte):
+#     for i in range(0,ymax):
+#         ligne = ''
+#         for j in range(xmin,xmax):
+#             if carte[i][j] == '+': ligne += '+'
+#             elif carte[i][j] == 0: ligne += '.'
+#             elif carte[i][j] == 1: ligne += '#'
+#             else: ligne += 'o'
+#         print(ligne)
 
-# Affichage
-def affiche(map):
-    for i in range(0,ymax):
-        ligne = ''
-        for j in range(xmin,xmax):
-            if map[i][j] == '+': ligne += '+'
-            elif map[i][j] == 0: ligne += '.'
-            elif map[i][j] == 1: ligne += '#'
-            else: ligne += 'o'
-        print(ligne)
+def onestep(carte,xs,ys):
+    if carte[ys+1][xs] == 0: return xs,ys+1
+    elif carte[ys+1][xs-1] == 0: return xs-1,ys+1
+    elif carte[ys+1][xs+1] == 0: return xs+1,ys+1
 
+def blocked(carte,xs,ys):
+    return carte[ys+1][xs] != 0 and carte[ys+1][xs-1] != 0 and carte[ys+1][xs+1] != 0
 
-
-def onestep(map,xs,ys):
-    if map[ys+1][xs] == 0: return xs,ys+1
-    elif map[ys+1][xs-1] == 0: return xs-1,ys+1
-    elif map[ys+1][xs+1] == 0: return xs+1,ys+1
-
-def blocked(map,xs,ys):
-    return map[ys+1][xs] != 0 and xs-1>=xmin-1 and map[ys+1][xs-1] != 0 and xs+1<=xmax and map[ys+1][xs+1] != 0
-
-def out(map,ys):
+def out(carte,ys):
     return ys >= ymax
 
-# Chute du sable
+## Chute du sable
 compteur = 0
 while True:
     xs,ys = xsource,ysource
-    while not blocked(map,xs,ys) and not out(map,ys):
-        xs, ys = onestep(map,xs,ys)
-    if out(map,ys):
+    while not blocked(carte,xs,ys) and not out(carte,ys):
+        xs, ys = onestep(carte,xs,ys)
+    if out(carte,ys):
         break
-    if blocked(map,xs,ys):
+    if blocked(carte,xs,ys):
         compteur += 1
+    carte[ys][xs] = -1
 
-    map[ys][xs] = -1
-
-affiche(map)
 print(compteur)
 
 
